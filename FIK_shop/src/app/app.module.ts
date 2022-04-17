7dd86779217bc6fd953c1d63f7aae2646d2559ac
Luz Aguirre <luz.aguirre.morales@accenture.com>
2022-04-17 13:17:08 +0200

    header component created

--- FIK_shop/src/app/app.module.ts
+++ FIK_shop/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopHeaderComponent } from './components/shop-header/shop-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopHeaderComponent
   ],
   imports: [
     BrowserModule,
