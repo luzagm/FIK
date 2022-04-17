b9589eb399f74bd7e37bdcc335c157b109be6b60
Luz Aguirre <luz.aguirre.morales@accenture.com>
2022-04-17 16:34:56 +0200

    grid style completed

--- FIK_shop/src/app/components/shop-content/shop-content.component.ts
+++ FIK_shop/src/app/components/shop-content/shop-content.component.ts
@@ -9,21 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class ShopContentComponent implements OnInit {
 
     products: Product[] = [];
+    filteredColor: FilterColor [] = [];
 
     constructor() { }
 
     ngOnInit(): void {
 
     this.products = [
-            {url:'../../../assets/Images/alex-iby-XhMSz5I1kn8-unsplash.jpg', name: 'BRIONI',  description: 'Brioni vintage inspired black sunglasses', model: 'Model Ref: 60BRI', price: 320.00 },
-            {url:'../../../assets/Images/woman-gc313e4feb_1920.jpg', name: 'BRIONI',  description: 'Brioni square glasses made with acetate and titanium', model: 'Model Ref: 48BRI', price: 490.00 },
-            {url:'../../../assets/Images/woman-ga205e16f2_1920.jpg', name: 'BRIONI',  description: 'Brioni titanium silver sunglasses', model: 'Model Ref: 59BRI', price: 460.00 },
-            {url:'../../../assets/Images/woman-ga29a8ad14_1920.jpg', name: 'BRIONI',  description: 'Brioni pilot light havana sunglasses', model: 'Model Ref: 53BRI', price: 3200.00 },
-            {url:'../../../assets/Images/alex-iby-XhMSz5I1kn8-unsplash.jpg', name: 'BRIONI',  description: 'Brioni rectangular brown and green sunglasses', model: 'Model Ref: 55BRI', price: 320.00 },
-            {url:'../../../assets/Images/model-gcecd4b72f_1920.jpg', name: 'BRIONI',  description: 'Brioni square vintage black acetate sunglasses', model: 'Model Ref: 56BRI', price: 350.00 },
-            {url:'../../../assets/Images/prince-akachi-4Yv84VgQkRM-unsplash.jpg', name: 'BRIONI',  description: 'Brioni titanium silver grey sunglasses', model: 'Model Ref: 58BRI', price: 460.00 },
-            {url:'../../../assets/Images/jonathan-borba-reiwBhNJQS0-unsplash.jpg', name: 'BRIONI',  description: 'Brioni rectangular oversize havana acetate sunglasses', model: 'Model Ref: 54BRI', price: 320.00 },
+            {url:'../../../assets/Images/alex-iby-XhMSz5I1kn8-unsplash.jpg', name: 'BRIONI',  description: 'Brioni vintage inspired black sunglasses', model: 'Model Ref: 60BRI', color: 'silver', price: 320.00 },
+            {url:'../../../assets/Images/woman-gc313e4feb_1920.jpg', name: 'BRIONI',  description: 'Brioni square glasses made with acetate and titanium', model: 'Model Ref: 48BRI', color: 'black', price: 490.00 },
+            {url:'../../../assets/Images/woman-ga205e16f2_1920.jpg', name: 'BRIONI',  description: 'Brioni titanium silver sunglasses', model: 'Model Ref: 59BRI', color: 'black', price: 460.00 },
+            {url:'../../../assets/Images/burlesque-ga37fa2390_1920.jpg', name: 'BURLESQUE',  description: 'Burlesque heartshaped red sunglasses', model: 'Model Ref: 59BRI', color: 'red', price: 460.00 },
+            {url:'../../../assets/Images/woman-ga29a8ad14_1920.jpg', name: 'BRIONI',  description: 'Brioni pilot light havana sunglasses', model: 'Model Ref: 53BRI', color: 'silver', price: 3200.00 },
+            {url:'../../../assets/Images/woman-gd5276871d_1920.jpg', name: 'BRIONI',  description: 'Brioni rectangular brown and green sunglasses', model: 'Model Ref: 55BRI', color: 'silver', price: 320.00 },
+            {url:'../../../assets/Images/luigi-estuye-lucreative-Nb_QBfC6Dfw-unsplash.jpg', name: 'LUIGI',  description: 'Luigi square red glasses', model: 'Model Ref: 55BRI', color: 'red', price: 320.00 },
+            {url:'../../../assets/Images/model-gcecd4b72f_1920.jpg', name: 'BRIONI',  description: 'Brioni square vintage black acetate sunglasses', model: 'Model Ref: 56BRI', color: 'black',  price: 350.00 },
+            {url:'../../../assets/Images/prince-akachi-4Yv84VgQkRM-unsplash.jpg', name: 'BRIONI',  description: 'Brioni titanium silver grey sunglasses', model: 'Model Ref: 58BRI', color: 'black', price: 460.00 },
+            {url:'../../../assets/Images/jonathan-borba-reiwBhNJQS0-unsplash.jpg', name: 'BRIONI',  description: 'Brioni rectangular oversize havana acetate sunglasses', model: 'Model Ref: 54BRI', color: 'black', price: 320.00 },
         ];
+
+        this.filteredColor = [
+            {color: 'black', id: 1},
+            {color: 'red', id: 2},   
+            {color: 'silver', id: 3}
+        ]
   }
 
 }
@@ -33,5 +42,11 @@ interface Product {
     name: string,
     description: string,
     model: string,
+    color: string,
     price: number,
+};
+
+interface FilterColor {
+    color: string,
+    id: number,
 };
